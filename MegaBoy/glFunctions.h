fd5ca47d4dd10f57ef9734e8bc167c648bfc4d6b
MeGaL0DoN <62940883+MeGaL0DoN@users.noreply.github.com>
2024-08-10 15:20:21 -0700

    Delete MegaBoy directory

--- MegaBoy/glFunctions.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <cstdint>
-
-namespace OpenGL
-{
-	void bindTexture(uint32_t textureId);
-	void createTexture(uint32_t& textureId, uint32_t width, uint32_t height, const uint8_t* data = nullptr);
-	void updateTexture(uint32_t textureId, uint32_t width, uint32_t height, const uint8_t* data);
-}
